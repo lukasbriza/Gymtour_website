e8eececa900cd285c6e998a5473f22bb0f87de91
Lu(c)ky/Coder <lukasbriza@seznam.cz>
2022-03-15 23:38:29 +0100

    config

--- server/Config/securityOptions.js
+++ server/Config/securityOptions.js
@@ -1,10 +1,11 @@
 const config = {
   jsonLimit: "10kb",
   rateLimit: {
-    maxRequests: 2,
+    maxRequests: 200,
     windowMS: 60 * 60 * 1000,
     message: "You reached maximum number of requests.",
   },
+  tokenExpiration: "10h",
 };
 
 module.exports = config;
